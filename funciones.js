0887e014aeb8c62b764471ed097f1bcf94a6a1e6
marianacruz29 <mariana.cruzfr@gmail.com>
2020-12-10 00:23:08 -0600

    Actividad12

---
+++ funciones.js
@@ -0,0 +1,11 @@
+function comparar(){
+    const va15 = Number(prompt("Ingresa el primer Valor:"));
+    const va16 = Number(prompt("Ingresa El segundo Valor:"));
+
+    if(va15>va16){
+        alert(va15 + " Es Mayor Que " + va16);
+    }else{
+        alert(va16 + " Es Mayor Que" + va15);
+    }
+
+}
